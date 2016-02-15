91fda3674fbaffc6f19c7c7440affa2df439bbb6
hojin chu <chjin990@gmail.com>
2016-02-15 16:52:33 +0900

    RandomTest 버전 통합중임. 기본적인 서버클라이언트 소켓통신 통합중임.

---
+++ src/com/common/Protocol.java
@@ -0,0 +1,47 @@
+package com.common;
+
+/**
+ * Created by cjw on 2016-02-15.
+ * 클라이언트 로그인 요청 ==>서버가 클라이언트에 응답 ==> 킄라이언트 로그인.
+ */
+public class Protocol {
+    //최초로그인 정보 100   (login.jsp)
+    public static final int LOGIN=100;
+
+    //로그인한 사람 처리 (login_ok.jsp)
+
+
+    //접속한 로그인 정보 출력
+    public static final int WAITROOMLOGIN=110;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
